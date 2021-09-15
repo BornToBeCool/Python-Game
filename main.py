99deeb051c66ee0d28194d37b30397aba5027de5
BornToBeCool <georgekanaridis@gmail.com>
2021-09-15 14:52:47 +0100

    Main function changed

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from game import *
-from team import *
-from run import *
-
-# Just testing
-# Just testing
 
 def main():
-    runGame()
-    
-
-
+    game = Game()
+    game.AP_or_AR()
+    while True:
+        if (len(game.teams[0].alive_heroes) == 0 or len(game.teams[1].alive_heroes) == 0):
+            break
+        print("Round " + str(game.game_round()))
+        print("---------------------------------------")
+        print(" ")
+        game.printHeroes()
+        game.calculate_and_apply_damages() 
 main() 
-
-
-
-
